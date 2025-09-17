93f1026d036ffd92537f48e43a4aee0e1a98010d
Jun Aishima <jaishima@bnl.gov>
2025-09-17 10:46:32 -0400

    remove commented-outline

--- run-deployment.py
+++ run-deployment.py
@@ -4,7 +4,6 @@ from prefect.deployments import run_deployment
 
 run_deployment(
     name="end-of-run-workflow/ucal-end-of-run-workflow-docker",
-    # name="end-of-run-workflow/end_of_run_workflow_deployment",
     parameters={"stop_doc": {"run_start": ""}},
     timeout=15,  # don't wait for the run to finish # edit to 15 sec
 )
