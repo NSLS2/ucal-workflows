170de382ce7c7abf48317abf5cd43d3be59a8d1b
Jun Aishima <jaishima@bnl.gov>
2025-10-02 14:28:09 -0400

    remove file not usable by beamline staff

--- run-deployment.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from prefect.deployments import run_deployment
-
-run_deployment(
-    name="end-of-run-workflow/ucal-end-of-run-workflow-docker",
-    parameters={"stop_doc": {"run_start": ""}},
-    timeout=15,  # don't wait for the run to finish # edit to 15 sec
-)
