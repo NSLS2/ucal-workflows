3fc90d20c3e11e21753601864bf3d5c0410d3462
Jun Aishima <jaishima@bnl.gov>
2025-10-06 15:20:38 -0400

    add fix to problem during deployment
      * got a coroutine error when attempting to do 'prefect deploy'. adding
        this kwarg to the load function was proposed by Prefect to fix this

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import os
 LOCATION="ucal"
 
 def get_tiled_client():
-    os.environ["TILED_API_KEY"] = Secret.load(f"tiled-{LOCATION}-api-key").get()
+    os.environ["TILED_API_KEY"] = Secret.load(f"tiled-{LOCATION}-api-key", _sync=True).get()
     tiled_client = from_profile("nsls2")[LOCATION]
     os.environ.pop("TILED_API_KEY")
     return tiled_client
