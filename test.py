f207934f32bcdce8088a767fe6913b7ce1785d45
Jun Aishima <jaishima@bnl.gov>
2026-01-08 11:49:19 -0500

    update test module
     * provide useful info about prefect, tiled, and pixi

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import prefect
+import subprocess
 import sys
-
+import tiled
 
 def test(stuff=""):
     if stuff:
@@ -10,7 +12,15 @@ def test(stuff=""):
         print("test: EMPTY")  # noqa: T201
 
 
+def info():
+    print(f"Prefect info: {prefect.__version_info__}")
+    print(f"Tiled info: {tiled.__version__}")
+    output = subprocess.call("pixi --version")
+    print(f"Pixi info: {output}")
+
+
 if __name__ == "__main__":
+    info()
     if len(sys.argv) > 1:
         test(sys.argv[1])
     else:
